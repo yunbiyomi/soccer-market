03b8f65dbad8674e7b86bf6df72f01f428c2d922
yunbiyomi <yunbiyomi@naver.com>
2023-12-26 22:00:51 +0900

    💄 Style : 비밀번호 input 디자인 수정

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,40 @@
 import React, { useState } from 'react'
-import logo from '../assets/soccerMarketLogo.png'
+import logo from '../assets/soccer-market-logo.png'
+import checkBox from '../assets/chek-disabled.svg'
 import styled from 'styled-components'
 import Button from '../components/common/Button/Button'
+import agreeCheck from '../assets/agree-check.svg'
+import agreeCheckFill from '../assets/agree-check-fill.svg'
 import axios from 'axios'
 
 const SignUpPage = () => {
   const url = 'https://openmarket.weniv.co.kr/';
   const [userId, setUserId] = useState('');
+  const [userIdRequired, setUserIdRequired] = useState(false);
+  const [userName, setUserName] = useState('');
   const [userIdAvailable, setUserIdAvailable] = useState(true);
   const [userIdMsg, setUserIdMsg] = useState('');
+  const [checked, setChecked] = useState(false);
 
   const onIdHandler = (e) => {
     setUserId(e.currentTarget.value);
   }
 
+  const onNameHandler = (e) => {
+    setUserName(e.currentTarget.value);
+  }
+  
+  const handleCheckboxChange = () => {
+    setChecked(!checked);
+  };
+
+  // 아이디 중복확인
   const handleCheckUserId = async () => {
+    if(!userId){
+      setUserIdRequired(true);
+      return;
+    }
+
     try {
       const response = await axios.post(url+`/accounts/signup/valid/username/`,{
         username:userId
@@ -42,25 +62,30 @@ const SignUpPage = () => {
         <SForm>
           <label htmlFor='id'>아이디</label>
           <IdWrap>
-            <SIdInput id='id' type="text" onChange={onIdHandler}/>
-            <Button width='120px' height='54px' fontSize='16px' fontWeight='regular' margin='0 0 0 12px' onClick={handleCheckUserId} disabled={!userId}>
+            <SIdInput id='id' type="text" onChange={onIdHandler} autoComplete='off' required/>
+            <Button width='120px' height='54px' fontSize='16px' fontWeight='regular' margin='0 0 0 12px' onClick={handleCheckUserId}>
               중복확인
             </Button>
           </IdWrap>
           {
-            userIdMsg.Success
-            ? (<CorrectMsg>{userIdMsg.Success}</CorrectMsg>)
-            : (<ErrorMsg>{userIdMsg.FAIL_Message}</ErrorMsg>)
+            userIdRequired && !userId ? (
+              <ErrorMsg>아이디는 필수 정보입니다.</ErrorMsg>
+            ) :
+              userIdMsg.Success
+              ? (<CorrectMsg>{userIdMsg.Success}</CorrectMsg>)
+              : (<ErrorMsg>{userIdMsg.FAIL_Message}</ErrorMsg>)
           }
-          
-          <label htmlFor='pw'>비밀번호</label>
-          <SInput id='pw' type="password" required/>
-
-          <label htmlFor='pwCheck'>비밀번호 재확인</label>
-          <SInput id='pwCheck' type="password" required/>
+          <PwWrap>
+            <label htmlFor='pw'>비밀번호</label>
+            <SPwInput id='pw' type="password" required/>
+          </PwWrap>
+          <PwWrap>
+            <label htmlFor='pwCheck'>비밀번호 재확인</label>
+            <SPwInput id='pwCheck' type="password" required/>
+          </PwWrap>
 
           <label htmlFor='name'>이름</label>
-          <SInput id='name' type="text" required/>
+          <SInput id='name' type="text" onChange={onNameHandler} autoComplete='off' required/>
 
           <label htmlFor='phone'>휴대폰번호</label>
           <PhoneNumberWrap>
@@ -72,21 +97,18 @@ const SignUpPage = () => {
               <option value="018">018</option>
               <option value="019">019</option>
             </SSelect>
-            <SPhoneInput id='phone-mid' type='tel'required/>
-            <SPhoneInput id='phone-end' type='tel'required/>
+            <SPhoneInput id='phone-mid' type='tel' autoComplete='off' required/>
+            <SPhoneInput id='phone-end' type='tel'autoComplete='off' required/>
           </PhoneNumberWrap>
         </SForm>
       </FormContainer>
       <BottomWrap>
-        <div>
-          <input type="checkbox" id="agree"/>
-          <label 
-            htmlFor="agree"
-            style={{marginLeft: '5px'}}
-          >
+        <AgreeBox>
+          <CheckInput type="checkbox" id="agree" checked={checked} onChange={handleCheckboxChange}/>
+          <label htmlFor="agree">
             싸커마켓의 <u>이용약관</u> 및 <u>개인정보처리방침</u>에 대한 내용을 확인하였고 동의합니다.
           </label>
-        </div>
+        </AgreeBox>
         <Button width='480px' height='60px' disabled>
           가입하기
         </Button> 
@@ -169,6 +191,37 @@ const SIdInput = styled(SInput)`
   width: 346px;
 `;
 
+const SPwInput = styled(SInput)`
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #313131;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+`;
+
+const PwWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    background: url(${checkBox}) center center / contain no-repeat;
+    bottom: 27.5px;
+    right: 15px;
+  }
+`;
+
 const PhoneNumberWrap = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -179,7 +232,7 @@ const PhoneNumberWrap = styled.div`
 
 const SSelect = styled.select`
   height: 54px;
-  margin: 10px 0 20px 0;
+  margin: 10px 0 15px 0;
   padding: 0 20px;
   border: 1px solid var(--gray);
   border-radius: 5px;
@@ -211,4 +264,24 @@ const CorrectMsg = styled.div`
 
 const ErrorMsg = styled(CorrectMsg)`
   color: var(--red);
+`;
+
+const AgreeBox = styled.div`
+  width: 480px;
+  display: flex;
+  justify-content: center;
+`;
+
+const CheckInput = styled.input`
+  margin-right: 10px;
+  appearance: none;
+  width: 16px;
+  height: 16px;
+  outline: none;
+  cursor: pointer;
+  background: url(${agreeCheck}) center center / contain no-repeat;
+
+  &:checked {
+    background: url(${agreeCheckFill}) center center / contain no-repeat;
+  }
 `;
