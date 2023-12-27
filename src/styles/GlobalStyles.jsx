d7930b958dca77f6c14fdd172fbe0a0f37b95962
yunbiyomi <yunbiyomi@naver.com>
2023-12-27 22:46:31 +0900

    ♻️ Refactor : 폰트 선언 방식 수정

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
-
   :root {
     --point-color: #001D82;
     --light-blue: #E7ECFF;
@@ -12,6 +10,13 @@ const GlobalStyles = createGlobalStyle`
     --light-gray: #F2F2F2;
     --disabled-gray: #E0E0E0
     --light-font-color: #767676;
+  }
+
+  @font-face {
+    font-family: 'SpoqaHanSansNeo-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
 
   * {
