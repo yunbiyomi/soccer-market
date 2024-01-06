9d46c98614711035c07976a2f6677fe164dd8233
yunbiyomi <yunbiyomi@naver.com>
2024-01-07 00:54:28 +0900

    ✨ Feat : Token Header에 저장

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,23 @@
 import axios from "axios";
+import { getCookie } from "../hooks/Cookies";
 
 const instance = axios.create({
   baseURL: 'https://openmarket.weniv.co.kr/',
 });
+
+instance.interceptors.request.use((config) => {
+  const token = getCookie('token');
+  try {
+    if(token){
+      config.headers.Authorization = token;
+    }
+    return config;
+  } catch (error) {
+    console.error('axios.interceptors.request: ', error);
+  }},
+  (error) => {
+    return Promise.reject(error);
+  }
+)
 
 export default instance
