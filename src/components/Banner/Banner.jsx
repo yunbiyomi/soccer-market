0a327855e5089df100a9280c05972c59db6018b4
yunbiyomi <yunbiyomi@naver.com>
2024-02-05 23:22:13 +0900

    ✨ Feat : Banner 이미지 슬라이드 구현

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './Banner.css'
+import Img1 from '../../assets/BannerImg/1.jpg'
+import Img2 from '../../assets/BannerImg/2.jpg'
+import Img3 from '../../assets/BannerImg/3.jpg'
+import Img4 from '../../assets/BannerImg/4.jpg'
+import Img5 from '../../assets/BannerImg/5.jpg'
+import Img6 from '../../assets/BannerImg/6.jpg'
+import Img7 from '../../assets/BannerImg/7.jpg'
+import Img8 from '../../assets/BannerImg/8.jpg'
+import Img9 from '../../assets/BannerImg/9.jpg'
+import Img10 from '../../assets/BannerImg/10.jpg'
+
+const Banner = () => {
+  return (
+    <BannerWrap>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation 
+        loop={true} 
+        pagination={{ clickable: true }} 
+        autoplay={{ delay: 5000 }} 
+      >
+        <SwiperSlide>
+          <SBanner src={Img1} alt='Tottenham'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img2} alt='Barcelona'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img3} alt='BayernMunchen'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img4} alt='Chelsea'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img5} alt='Liverpool'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img6} alt='ManchesterUnited'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img7} alt='ManchesterCity'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img8} alt='FCKorea'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img9} alt='Arsenal'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SBanner src={Img10} alt='RealMadrid'/>
+        </SwiperSlide>
+      </Swiper>
+    </BannerWrap>
+  )
+}
+
+export default Banner
+
+const BannerWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SBanner = styled.img`
+  width: 100%;
+  height: 500px;
+  object-fit: cover;
+  margin-top: 2px;
+`;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/common/Header/Header'
 import ProductContainer from '../components/Product/ProductContainer'
 import Footer from '../components/common/Footer/Footer'
+import Banner from '../components/Banner/Banner'
 
 const MainPage = () => {
   return (
     <>
       <Header />
+      <Banner />
       <ProductContainer />
       <Footer />
     </>
