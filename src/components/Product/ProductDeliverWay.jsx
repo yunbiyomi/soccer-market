f0b83ac2c1ea4908ae414e4efeb308e2ab8e5fde
yunbiyomi <yunbiyomi@naver.com>
2024-02-15 00:47:11 +0900

    ♻️ Refactor : 배송 방법/ 배송비 component 분리

---
+++ src/components/Product/ProductDeliverWay.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+import useCommaFormat from '../../hooks/useCommaFormat';
+
+const ProductDeliverWay = ({ shippingMethod, shippingFee, textAlign, fontSize, marginBottom }) => {
+  const delivery = shippingMethod === 'PARCEL' ? '택배배송' : '화물배송';
+  const fee = useCommaFormat(shippingFee);
+
+  return (
+    <DeliverWay fontSize={fontSize} textAlign={textAlign} marginBottom={marginBottom}>
+      {delivery} / {fee === '0' ? '무료배송' : `${fee}원`}
+    </DeliverWay>
+  )
+}
+
+export default ProductDeliverWay
+
+const DeliverWay = styled.p`
+  text-align: ${(props) => props.textAlign || 'start'};;
+  font-size: ${(props) => props.fontSize || '16px'};
+  color: var(--light-font);
+  margin-bottom: ${(props) => props.marginBottom || '20px'};
+`;
