a94c0da358583148e36cce8bf96fef6f09522055
yunbiyomi <yunbiyomi@naver.com>
2024-02-19 22:41:27 +0900

    🐛 Fix : 렌더링 두번씩 되는 오류 수정

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import store from "./store/store";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
