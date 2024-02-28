c8b539f457c3065645e7af37683a92648997f574
yunbiyomi <yunbiyomi@naver.com>
2024-02-28 22:24:10 +0900

    ✨ Feat : 검색어 강조 기능 추가

--- src/components/common/Header/InputBox.jsx
+++ src/components/common/Header/InputBox.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
-import SearchIcon from '../../../assets/icon-search.svg'
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+import SearchIcon from '../../../assets/icon-search.svg';
 import axios from '../../../api/axios';
 import { Link } from 'react-router-dom';
 
 const InputBox = () => {
-  const [searchTerm, setSearchTerm] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
   const [searchData, setSearchData] = useState([]);
 
+  // 검색어 저장
   const searchHandler = (e) => {
     const value = e.currentTarget.value;
     setSearchTerm(value);
-  }
+  };
 
+  // 검색 결과 가져오기
   const getSearchProduct = async () => {
     try {
       const response = await axios.get(`products/?search=${searchTerm}`);
@@ -20,49 +22,59 @@ const InputBox = () => {
     } catch (error) {
       console.error('상품 검색 실패', error.response.data);
     }
-  }
+  };
 
   useEffect(() => {
-    if (searchTerm === '') 
-      setSearchData([]);
-    else
-      getSearchProduct();
-    console.log(searchData);
-  }, [searchTerm])
+    if (searchTerm === '') setSearchData([]);
+    else getSearchProduct();
+  }, [searchTerm]);
+
+  // 검색어랑 검색 결과에서 같은 단어 표시
+  const highlightText = (text, searchTerm) => {
+    const regex = new RegExp(`(${searchTerm})`, 'gi');
+    return text.split(regex).map((part, index) => {
+      return regex.test(part) ? 
+      (
+        <Highlight key={index}>
+          {part}
+        </Highlight>
+      ) : (
+        <span key={index}>
+          {part}
+        </span>
+      );
+    });
+  };
 
   return (
-    <InputWrap> 
-      <SSearchInput 
-        placeholder='상품을 검색해보세요!' 
+    <InputWrap>
+      <SSearchInput
+        placeholder="상품을 검색해보세요!"
         onChange={searchHandler}
       />
       <SearchButton>
         <SSearchIcon src={SearchIcon} alt="Search" />
       </SearchButton>
-      {
-        searchData.length > 0 && (
-          <SearchDataList>
-            {searchData.map((product) => (
-              <SSearchData key={product.product_id}>
-                <SearchDataLink
-                  to={`/detail?id=${product.product_id}`}
-                >
-                  {product.product_name}
-                </SearchDataLink>
-              </SSearchData>
-            ))}
-          </SearchDataList>
-        )
-      }
+      {searchData.length > 0 && (
+        <SearchDataList>
+          {searchData.map((product) => (
+            <SSearchData key={product.product_id}>
+              <SearchDataLink to={`/detail?id=${product.product_id}`}>
+                {highlightText(product.product_name, searchTerm)}
+              </SearchDataLink>
+            </SSearchData>
+          ))}
+        </SearchDataList>
+      )}
     </InputWrap>
-  )
-}
+  );
+};
 
-export default InputBox
+export default InputBox;
 
 const InputWrap = styled.div`
   position: relative;
-  display: inline-block;  
+  display: inline-block;
 `;
 
 const SSearchInput = styled.input`
@@ -89,7 +101,7 @@ const SSearchInput = styled.input`
 const SearchButton = styled.button`
   position: absolute;
   top: 50%;
-  right: 15px; 
+  right: 15px;
   transform: translateY(-50%);
   background: none;
   border: none;
@@ -102,7 +114,7 @@ const SearchButton = styled.button`
 `;
 
 const SSearchIcon = styled.img`
-  width: 25px; 
+  width: 25px;
   height: 25px;
   object-fit: cover;
 `;
@@ -130,17 +142,28 @@ const SearchDataList = styled.ul`
   }
 `;
 
+const SearchDataLink = styled(Link)`
+  margin: 10px;
+`;
+
 const SSearchData = styled.li`
   padding: 5px 0;
   border-bottom: 1px solid var(--gray);
-`;
-
-const SearchDataLink = styled(Link)`
-  margin: 10px;
   cursor: pointer;
 
   &:hover {
-    color: var(--point-color);
-    text-decoration: underline;
+    background-color: var(--light-point-color);
+
+    ${SearchDataLink} {
+      color: var(--point-color);
+      font-weight: bold;
+      text-decoration: underline;
+      background-color: var(--light-point-color);
+    }
   }
+`;
+
+const Highlight = styled.span`
+  color: var(--point-color);
+  font-weight: bold;
 `;
